7f5d6eb55991f6493099d83ca99030353419fe31
wkmghn <tailshade@hotmail.com>
2016-08-15 18:19:48 +0900

    ライセンス表記を追加

--- test/sarc_test.cpp
+++ test/sarc_test.cpp
@@ -1,6 +1,10 @@
-﻿// sarc_test.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+﻿//
+// Copyright (c) 2016 wkmghn.
+// 
+// Use, modification and distribution is subject to the Boost Software License,
+// Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
 //
-
 #define _CRT_SECURE_NO_WARNINGS
 
 #include "stdafx.h"
